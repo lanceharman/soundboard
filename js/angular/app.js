b92ad2017f59fd67121736463e20423510046af3
lanceharman <lanceharman@gmail.com>
2016-03-02 20:36:31 -0500

    Initial commit

---
+++ js/angular/app.js
@@ -0,0 +1,36 @@
+(function() {
+
+	var app = angular.module('app', ['ui.router', 'firebase']);
+
+	app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider){
+
+		$urlRouterProvider.otherwise('/');
+
+		$stateProvider.state('home',
+		{
+			url: '/',
+			templateUrl:'views/home/home.html',
+			controller:'SoundCtrl'
+		}).
+		state('upload',
+		{
+			url: '/upload',
+			templateUrl:'views/upload/upload.html',
+			controller:'UploadCtrl'
+		}).
+		state('config',
+		{
+			url: '/config',
+			templateUrl:'views/config/config.html',
+			controller:'ConfigCtrl'
+		}).
+		state('sign',
+		{
+			url: '/sign',
+			templateUrl:'views/sign/sign.html',
+			controller:'SignCtrl'
+		});
+
+	}]);
+
+})();
---
+++ upload.php
@@ -0,0 +1,29 @@
+<?php
+	try{
+		if (isset($_FILES['upload_file'])) {
+
+			$white_list = array("mp3", "wav", "ogg");
+			$file_path = $_FILES['upload_file']['name'];
+			$ext = pathinfo($file_path, PATHINFO_EXTENSION);
+
+			if(! in_array($ext, $white_list)){
+				throw new Exception("Unknown File Type");
+			}
+
+		    if(move_uploaded_file($_FILES['upload_file']['tmp_name'], "sounds/" . $_FILES['upload_file']['name'])){
+		        echo $_FILES['upload_file']['name']. " Uploaded Successfully";
+		    } else {
+		        echo $_FILES['upload_file']['name']. " Upload Failed!";
+		    }
+
+		    exit;
+
+		} else {
+		    echo "No files uploaded ...";
+		}
+	}
+
+	catch(Exception $e){
+		exit($e -> getMessage());
+	}
+?>
